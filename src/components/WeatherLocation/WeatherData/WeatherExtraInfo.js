f7c72be798dd45cff37c136cd86d8021c2de6f8b
Azael Martinez <azael950620@gmail.com>
2019-08-23 15:47:58 -0500

    Se agrega PropType.Shape a WeatherData validando la variable Data

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -4,8 +4,8 @@ import './styles.css';
 
 const WeatherExtraInfo = ({ humidity, wind }) => (
     <div className="weather-extra-info-cont">
-        <span className="extraInfoText">{`Humedad: ${humidity} % - `}</span>
-        <span className="extraInfoText">{`Vientos: ${wind} viento`}</span>
+        <span className="extraInfoText">{`Humedad: ${humidity} %`}</span>
+        <span className="extraInfoText">{`Vientos: ${wind}`}</span>
     </div>
 );
 WeatherExtraInfo.propTypes = {
