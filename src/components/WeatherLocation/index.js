b29b4913ac4ee53bde6d10271958a1ce3c94d0e0
Azael Martinez <azael950620@gmail.com>
2019-08-27 16:48:37 -0500

    Agregando efecto de Cargando... con Material UI

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Location from './Location'; 
 import WeatherData from './WeatherData'; 
 import './styles.css';
 import API from  './../../services/API';
 import APIKEY from './../../services/weathers/APIKEY';
-import { RAIN } from '../../constants/weathers';
 import transformWeather from './../../plugins/transformWeather';
-
-const data = {
-    temperature: 25,
-    weatherState: RAIN,
-    humidity: 10,
-    wind: '10 m/s',
-}
 
 class WeatherLocation extends Component {
     constructor() {
         super();
-        this.state = { city: 'Monclova, Coahuila', data };
+        this.state = { city: 'Monclova, Coahuila', data: null};
     }
 
     handleUpdateClick = () => {
@@ -30,13 +23,19 @@ class WeatherLocation extends Component {
         }); 
     }
 
+    componentDidMount() {
+        this.handleUpdateClick();
+    }
+
     render() {
         const { city, data } = this.state;
         return (
             <div className="weather-location">
                 <Location city={city}></Location>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
+                {data ? 
+                    <WeatherData data={data}></WeatherData> :
+                    <CircularProgress></CircularProgress>
+                }
             </div>            
         );
     }
