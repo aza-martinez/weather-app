664c15c5bb40f85b9d102363a303656a930686ec
Azael Martinez <azael950620@gmail.com>
2019-08-27 12:54:15 -0500

    Refactorizando un poco

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import Location from './Location'; 
 import WeatherData from './WeatherData'; 
 import './styles.css';
-import { RAIN, SUN } from '../../constants/weathers';
 import API from  './../../services/API';
 import APIKEY from './../../services/weathers/APIKEY';
+import { RAIN } from '../../constants/weathers';
+import transformWeather from './../../plugins/transformWeather';
 
 const data = {
     temperature: 25,
@@ -19,27 +20,10 @@ class WeatherLocation extends Component {
         this.state = { city: 'Monclova, Coahuila', data };
     }
 
-    getWeatherState = () => { return SUN }
-
-    getData = weather_data => {
-        const { humidity, temp} = weather_data.main;
-        const { speed } = weather_data.wind;
-        const weatherState =  SUN;
-        const newData = {
-            humidity,
-            temperature: temp,
-            wind: speed,
-            weatherState,
-        };
-
-        return newData;
-        
-    }
-
     handleUpdateClick = () => {
         API.get(`/weather?q=London,uk&appid=${APIKEY}`)
         .then((response) => {
-           const newWeather = this.getData(response.data);
+           const newWeather = transformWeather(response.data);
            this.setState({ data: newWeather }); 
         }).catch((error) => {
             console.log(error);
